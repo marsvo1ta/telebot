6123763c021b3d0856887ded60ff4bbefd7de552
marsvo1ta <euromobilco@gmail.com>
2022-04-04 20:08:59 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+
+
+BOT_TOKEN = "5255176696:AAFMKYN9vEodLcEdj-uyoGQXH41H_POORRQ"
+
+async def start_handler(event: types.Message):
+    await event.answer(
+        f"Hello, {event.from_user.get_mention(as_html=True)} 👋!",
+        parse_mode=types.ParseMode.HTML,
+    )
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    try:
+        disp = Dispatcher(bot=bot)
+        disp.register_message_handler(start_handler, commands={"start", "restart"})
+        await disp.start_polling()
+    finally:
+        await bot.close()
+
+asyncio.run(main())
---
+++ telebot/credentials.py
@@ -0,0 +1,6 @@
+
+
+
+bot_token = "5255176696:AAFMKYN9vEodLcEdj-uyoGQXH41H_POORRQ"
+bot_user_name = "marsvolta32923_bot"
+URL = "ссылка на приложение heroku, которую мы создадим позже"
